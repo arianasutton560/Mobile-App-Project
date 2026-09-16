8b8fe0ff9d8153adf31c44283b00db6f5e87ab96
Ariana Sutton <agsutton@smu.edu>
2026-09-16 14:58:13 -0500

    update app.js

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
+import Favorites from './screens/Favorites';
 import RecipeDetails from './screens/RecipeDetails';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [favorites, setFavoriteIds] = useState([]);
+
+  function toggleFavorite(recipeId) {
+    setFavoriteIds((currentIds) => currentIds.includes(recipeId) ? 
+    currentIds.filter((id) => id !== recipeId) : 
+    [...currentIds, recipeId]
+  );
+}
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="Home" options={{ headerShown: false}}>
+          {(props) => (
+            <Home{...props}
+            favoriteIds={favoriteIds}
+            onToggleFavorite={toggleFavorite}
+            />
+          )}
+        </Stack.Screen>
+
+        <Stack.Screen 
+        name="Favorites"
+        options={{ title: 'Favorites' }}>
+        {(props) => (
+          <Favorites
+            {...props}
+            favoriteIds={favoriteIds}
+            onToggleFavorite={toggleFavorite}
+          />
+        )}
+        </Stack.Screen>
+
         <Stack.Screen
           name="RecipeDetails"
           component={RecipeDetails}
